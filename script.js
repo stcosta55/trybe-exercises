3d2bf8038b0df3e2718aaaa77b4eea5aeab42b5f
Su Costa <stcosta55@gmail.com>
2021-11-17 15:30:05 -0300

    exercicios aula 5.1

---
+++ script.js
@@ -0,0 +1,17 @@
+const paragraph = document.getElementById("paragraph");
+paragraph.style.color = "red";
+//console.log(document.getElementById("page-title").innerText = "Strange Fruit");
+//console.log(document.getElementById("second-paragraph").innerText = "Billie Holiday");
+//console.log(document.getElementById("subtitle").innerText = "Drama, Musical, reallife");
+
+// let paragrafos = (document.getElementsByTagName("p"));
+// for (let cont = 0; cont < paragrafos.length; cont += 1){
+//     console.log(document.getElementsByTagName("p")[cont].innerText = "filmes musicais");
+// }
+
+// console.log(document.getElementsByClassName("paragrafo")[0].innerText = "Billie Holiday");
+// console.log(document.getElementsByClassName("paragrafo")[1].style.color = "blue");
+
+// console.log(document.getElementsByTagName("p")[1].style.color = "red");
+
+console.log(document.querySelectorAll('p')[0].innerText = "query selector");
