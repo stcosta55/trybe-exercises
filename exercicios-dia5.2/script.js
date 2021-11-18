19fef56d36c9b1180f01c79a15f9a7ffab0b0fbb
Su Costa <stcosta55@gmail.com>
2021-11-18 15:29:02 -0300

    dia 5.2 - exercicio 1 - buscando elementos

---
+++ exercicios-dia5.2/script.js
@@ -0,0 +1,30 @@
+
+//aula 5.2 - exercicio 1 - buscando elementos
+// // Acesse o elemento elementoOndeVoceEsta .
+// console.log(document.getElementById("elementoOndeVoceEsta").innerText = "acessado elemento onde estou");
+
+// // Acesse pai a partir de elementoOndeVoceEsta e adicione uma color a ele.
+// console.log(document.getElementById("elementoOndeVoceEsta").parentNode.style.color = "red");
+
+// // Acesse o primeiroFilhoDoFilho e adicione um texto a ele. Você se lembra dos vídeos da aula anterior, como fazer isso?
+// console.log(document.getElementById("primeiroFilhoDoFilho").innerText = "texto");
+
+// // Acesse o primeiroFilho a partir de pai .
+// console.log(document.getElementById('pai').firstElementChild);
+
+// // Agora acesse o primeiroFilho a partir de elementoOndeVoceEsta .
+// console.log(document.getElementById("elementoOndeVoceEsta").firstElementChild);
+
+// // Agora acesse o texto Atenção! a partir de elementoOndeVoceEsta .
+// console.log(document.getElementById("elementoOndeVoceEsta").nextSibling);
+
+// // Agora acesse o terceiroFilho a partir de elementoOndeVoceEsta .
+// console.log(document.getElementById("elementoOndeVoceEsta").nextElementSibling);
+
+// // Agora acesse o terceiroFilho a partir de pai .
+// console.log(document.getElementById("pai").lastElementChild.previousElementSibling);
+
+
+
+
+//aula 5.2 - exercicio 2 - criando elementos
