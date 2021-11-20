c7b2b89684767b695f6c54ca34d437a6419e4709
Su Costa <stcosta55@gmail.com>
2021-11-20 12:11:07 -0300

    exercicio dia 5.3

--- exercicios-dia5.3/main.js
+++ exercicios-dia5.3/main.js
@@ -8,16 +8,49 @@ const myWebpage = document.getElementById('my-spotrybefy');
 // 1. Copie esse arquivo e edite apenas ele;
 // 1.1. Antes de começar os exercícios, use o LiveServer para dar uma olhada em como está a página no navegador.
 // 1.2. Note que uma das caixas está um pouco acima das outras. Por que isso ocorre?
+//pq ao primeiro LI possui a classes abaixo
+//( CSS function repositions an element vertically on the 2D plane. Its result is a <transform-function> data type.)
+// .tech {
+//     transform: translateY(-20px);
+//   }
+
 
 // 2. Crie uma função que adicione a classe 'tech' ao elemento `li` quando for clicado.
 // 2.1. Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?
+let clickBox = document.getElementsByTagName('li');
+console.log(clickBox);
+clickBox[0].addEventListener("click", recebeClick);
+clickBox[1].addEventListener("click", recebeClick);
+clickBox[2].addEventListener("click", recebeClick);
+
+function recebeClick(eventoDeOrigem){
+    //console.log(eventoDeOrigem)
+    if(eventoDeOrigem.target === clickBox[0]){
+        clickBox[1].className = null;
+        clickBox[2].className = null;
+        clickBox[0].className = 'tech';
+    } else if(eventoDeOrigem.target === clickBox[1]){
+        clickBox[0].className = null;
+        clickBox[2].className = null;
+        clickBox[1].className = 'tech';
+    } else {
+        clickBox[0].className = null;
+        clickBox[1].className = null;
+        clickBox[2].className = 'tech';
+    }
+}
+
 
 // 3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
 // com a classe 'tech';
 
+
+
 // 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
 // redirecione para alguma página;
 // 4.1. Que tal redirecionar para seu portifólio?
+
+
 
 // 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
 // a cor do mesmo;
